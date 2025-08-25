3211ab682754d0a030ec85465076125154bdc041
user <user@MacBook-Pro-dawid.local>
2025-08-25 21:16:16 +0200

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -126,8 +126,8 @@ vim.lsp.enable({
   -- "prismals",
   -- "pyright",
   -- "clangd",
-  "dockerls",
-  "docker_compose_language_service",
+  -- "dockerls",
+  -- "docker_compose_language_service",
   -- "helm_ls",
   -- "pyright",
   -- "rust_analyzer",
@@ -242,5 +242,32 @@ vim.cmd.Dotenv(vim.fn.stdpath("config") .. "/.env")
 
 require('dawid')
 
+vim.lsp.config('vtsls', {
+  cmd = { 'vtsls', '--stdio' },
+  filetypes = {
+    'javascript',
+    'javascriptreact',
+    'javascript.jsx',
+    'typescript',
+    'typescriptreact',
+    'typescript.tsx',
+  },
+  root_dir = function(bufnr, on_dir)
+    -- The project root is where the LSP can be started from
+    -- As stated in the documentation above, this LSP supports monorepos and simple projects.
+    -- We select then from the project root, which is identified by the presence of a package
+    -- manager lock file.
+    local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+    -- Give the root markers equal priority by wrapping them in a table
+    root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers } or root_markers
+    local project_root = vim.fs.root(bufnr, root_markers)
+    if not project_root then
+      return
+    end
+
+    on_dir(project_root)
+  end,
+})
+--
 --
 -- Snacks.picker.colorschemes()
--- .config/nvim/lua/dawid/workspaces/respo.lua
+++ .config/nvim/lua/dawid/workspaces/respo.lua
@@ -1,17 +1,17 @@
-vim.o.expandtab = false
+-- vim.o.expandtab = false
 
 vim.keymap.set('n', '<leader>gn', function()
-	Snacks.picker.grep({ dirs = { "/Users/user/work/respo/apps/native/" } })
-end, {desc = "Grep respo - native"})
+  Snacks.picker.grep({ dirs = { "/Users/user/work/respo/apps/native/" } })
+end, { desc = "Grep respo - native" })
 
 vim.keymap.set('n', '<leader>gff', function()
-	Snacks.picker.grep({ dirs = { "/Users/user/work/respo/apps/form/" } })
-end, {desc = "Grep respo - form"})
+  Snacks.picker.grep({ dirs = { "/Users/user/work/respo/apps/form/" } })
+end, { desc = "Grep respo - form" })
 
 vim.keymap.set('n', '<leader>gfk', function()
-	Snacks.picker.grep({ dirs = { "/Users/user/work/respo/apps/form-kids/" } })
-end, {desc = "Grep respo - form-kids"})
+  Snacks.picker.grep({ dirs = { "/Users/user/work/respo/apps/form-kids/" } })
+end, { desc = "Grep respo - form-kids" })
 
 vim.keymap.set('n', '<leader>ga', function()
-	Snacks.picker.grep({ dirs = { "/Users/user/work/respo/apps/api/" } })
-end, {desc = "Grep respo - api"})
+  Snacks.picker.grep({ dirs = { "/Users/user/work/respo/apps/api/" } })
+end, { desc = "Grep respo - api" })
